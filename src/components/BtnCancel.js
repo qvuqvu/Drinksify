c9964f778f91b7126615c483bcc4df8911a0694c
ThangNMT <114353857+ThangNMT@users.noreply.github.com>
2023-02-07 16:23:57 +0700

    Update BtnCancel.js

--- src/components/BtnCancel.js
+++ src/components/BtnCancel.js
@@ -37,5 +37,27 @@ export default BtnCancel = ({orderID}) => {
           historyID = doc.id;
         });
       });
+     await fireStore().collection('OrderHistories').doc(historyID).update({
+      cancelledTime: fireStore.Timestamp.now(),
+    });
+    await fireStore().collection('Orders').doc(orderID).update({
+      state: 'cancelled',
+    });
+    dispatch(setFalseWaitForDelete());
+    showMessage({
+      message: t('Order canceled successfully'),
+      description: t('Drinksify is pleased to serve you'),
+      type: 'danger',
+    });
+  };
+  return (
+    <TouchableOpacity
+      onPress={createTwoButtonAlert}
+      style={{backgroundColor: COLORS.custom, borderRadius: 10}}>
+      <Text style={styles.textAction}>{t('Cancel')}</Text>
+    </TouchableOpacity>
+  );
+};
+
 
    
